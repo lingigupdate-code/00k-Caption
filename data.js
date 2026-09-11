fb93551b73325917ea90284b229f1c084deb2287
lingigupdate-code <lingigupdate@gmail.com>
2026-09-12 01:28:58 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -112586,7 +112586,7 @@ const defaultData = {
         "brand": "CalvinKlein",
         "campaign": "NYFW",
         "platform": "X",
-        "hashtags": "#LinglingKwongxCalvinKlein \n#Linglingkwong \n#CalvinKleinSS27 \n#CalvinKlein #NYFW \n@CalvinKlein @XFashion @linglingsirilak"
+        "hashtags": "LINGLING APAC FACE OF CK \n#CalvinKleinSS27xLINGLING \n#LinglingKwongxCalvinKlein \n#Linglingkwong \n#CalvinKleinSS27 \n#CalvinKlein #NYFW \n@CalvinKlein @XFashion @linglingsirilak"
     }
 ]
 }
